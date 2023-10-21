2ad4e42c06620e2b1dbf26f0dc28d91f90ab38f0
Nilson Filho <104315653+nrmendf@users.noreply.github.com>
2023-10-21 12:12:43 -0300

    Add files via upload
    Controle de Fluxo - Desafio
    Vamos exercitar todo o conteúdo apresentado no módulo de Controle de Fluxo codificando o seguinte cenário.
    
    O sistema deverá receber dois parâmetros via terminal que representarão dois números inteiros, com estes dois números você deverá obter a quantidade de interações (for) e realizar a impressão no console (System.out.print) dos números incrementados, exemplo:
    
    Se você passar os números 12 e 30, logo teremos uma interação (for) com 18 ocorrências para imprimir os números, exemplo: "Imprimindo o número 1", "Imprimindo o número 2" e assim por diante.
    Se o primeiro parâmetro for MAIOR que o segundo parâmetro, você deverá lançar a exceção customizada chamada de ParametrosInvalidosException com a segunda mensagem: "O segundo parâmetro deve ser maior que o primeiro"
    Crie o projeto DesafioControleFluxo
    Dentro do projeto, crie a classe Contador.java para realizar toda a codificação do nosso programa.
    Dentro do projeto, crie a classe ParametrosInvalidosException que representará a exceção de negócio no sistema.
    Abaixo temos um trecho de código no qual você poderá seguir alterando as partes que contenham ??

---
+++ Contador.java
@@ -0,0 +1,38 @@
+package contador;
+
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        try {
+            System.out.println("Digite o primeiro parâmetro");
+            int parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro");
+            int parametroDois = terminal.nextInt();
+
+            // chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+
+        } catch (ParametrosInvalidosException e) {
+            // imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+            System.out.println(e.getMessage());
+        } finally {
+            // Fechar o Scanner para evitar o vazamento de recursos
+            terminal.close();
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        // validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm + 1;
+        // realizar o for para imprimir os números com base na variável contagem
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
---
+++ ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package contador;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
